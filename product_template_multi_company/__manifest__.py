278042d19a7c710c117d7a413aa62398440f180f
Dave Lasley <dave@laslabs.com>
2017-06-07 12:08:19 -0700

    Rename to product_template_multi_company

--- product_template_multi_company/__manifest__.py
+++ product_template_multi_company/__manifest__.py
@@ -3,8 +3,9 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': "Product multi-company",
-    'summary': "Select individually the product visibility on each company",
+    'name': "Product Template multi-company",
+    'summary': "Select individually the product template visibility on each "
+               "company",
     'author': "Tecnativa,"
               "Odoo Community Association (OCA)",
     'website': "https://www.tecnativa.com",
